e46a2b329be5eb964dd14d580c8584869027dcb6
peanut <lukun1@tal.com>
2022-07-09 14:36:19 +0800

    co php

--- basic/coroutine/coroutine.php
+++ basic/coroutine/coroutine.php
@@ -1,8 +1,6 @@
 <?php
 function phpfab($max){
-    $n=0;
-    $a=0;
-    $b=1;
+    list($n,$a,$b)=array(0,0,1);
     while ($n < $max){
         // 注意python的 a,b=b,a+b 和 a=b b=a+b 的区别
         // https://blog.csdn.net/qq_41187256/article/details/79687084
